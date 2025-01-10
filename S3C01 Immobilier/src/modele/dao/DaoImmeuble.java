a563a9a51006939b7be60affe27387d8a6b375d3
Nolan GILBERT <nolanofrance@gmail.com>
2025-01-10 11:23:27 +0100

    immeuble
    Daoimmeuble

---
+++ S3C01 Immobilier/src/modele/dao/DaoImmeuble.java
@@ -0,0 +1,81 @@
+package modele.dao;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import modele.Immeuble;
+import modele.dao.requetes.delete.RequeteDeleteImmeuble;
+import modele.dao.requetes.select.RequeteSelectImmeuble;
+import modele.dao.requetes.select.RequeteSelectImmeubleByID;
+import modele.dao.requetes.update.RequeteUpdateImmeuble;
+
+public class DaoImmeuble implements Dao<Immeuble> {
+
+    private Connection connection;
+
+    public DaoImmeuble(Connection connection) {
+        this.connection = connection;
+    }
+
+    @Override
+    public void create(Immeuble donnees) throws SQLException {
+        String sql = "INSERT INTO Immeuble (Id_Immeuble, Adresse) VALUES (?, ?)";
+        try (PreparedStatement prSt = connection.prepareStatement(sql)) {
+            prSt.setInt(1, donnees.getIdImmeuble());
+            prSt.setString(2, donnees.getAdresse());
+            prSt.executeUpdate();
+        }
+    }
+
+    @Override
+    public void update(Immeuble donnees) throws SQLException {
+        RequeteUpdateImmeuble requeteUpdate = new RequeteUpdateImmeuble();
+        try (PreparedStatement prSt = connection.prepareStatement(requeteUpdate.requete())) {
+            requeteUpdate.parametres(prSt, donnees);
+            prSt.executeUpdate();
+        }
+    }
+
+    @Override
+    public void delete(Immeuble donnees) throws SQLException {
+        RequeteDeleteImmeuble requeteDelete = new RequeteDeleteImmeuble();
+        try (PreparedStatement prSt = connection.prepareStatement(requeteDelete.requete())) {
+            requeteDelete.parametres(prSt, donnees);
+            prSt.executeUpdate();
+        }
+    }
+
+    @Override
+    public Immeuble findById(String... id) throws SQLException {
+        RequeteSelectImmeubleByID requeteSelectById = new RequeteSelectImmeubleByID();
+        try (PreparedStatement prSt = connection.prepareStatement(requeteSelectById.requete())) {
+            requeteSelectById.parametres(prSt, id);
+            try (ResultSet rs = prSt.executeQuery()) {
+                if (rs.next()) {
+                    return new Immeuble(
+                        rs.getInt("Id_Immeuble"),
+                        rs.getString("Adresse")
+                    );
+                }
+            }
+        }
+        return null; // Retourne null si aucun immeuble trouvé
+    }
+
+    @Override
+    public List<Immeuble> findAll() throws SQLException {
+        RequeteSelectImmeuble requeteSelectAll = new RequeteSelectImmeuble();
+        List<Immeuble> immeubles = new ArrayList<>();
+        try (PreparedStatement prSt = connection.prepareStatement(requeteSelectAll.requete());
+             ResultSet rs = prSt.executeQuery()) {
+            while (rs.next()) {
+                immeubles.add(new Immeuble(
+                    rs.getInt("Id_Immeuble"),
+                    rs.getString("Adresse")
+                ));
+            }
+        }
+        return immeubles;
+    }
+}
---
+++ S3C01 Immobilier/src/vue/ElementsSelectionnables.java
@@ -0,0 +1,18 @@
+package vue;
+
+public enum ElementsSelectionnables {
+    LOCATAIRE,
+    INDEX_COMPTEUR,
+    ASSUREUR,
+    ENTREPRISE,
+    IMMEUBLE,
+    TAXE,
+    LOUABLE,
+    FACTURE,
+    DIAGNOSTIC,
+    CONTRAT_DE_LOCATION,
+    CHARGE,
+    QUITTANCES,
+    LOGEMENT,
+    GARAGE
+}
